0297c93c55506a51156a3a30e4bb7dea932bf17f
MiKinshu <mishraprateekaries@gmail.com>
2019-08-31 03:36:34 +0530

    UI changes

--- app/src/main/java/com/kinshuu/attentify/MainActivity.java
+++ app/src/main/java/com/kinshuu/attentify/MainActivity.java
@@ -1,13 +1,63 @@
 package com.kinshuu.attentify;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference msubjectsDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+    //managing user data
+    String Subject="null";
+    String LTP="null";
+    String Prof="null";
+    String Date="null";
+    String Rollno="null";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseDatabase=FirebaseDatabase.getInstance();
+        msubjectsDatabaseReference=mFirebaseDatabase.getReference().child(Prof).child(Subject).child(LTP).child(Date);
+        mChildEventListener= new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
     }
 }
